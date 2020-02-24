f21e7f33d34642c934e9992350623649b7229770
Lucas Souza <lucas.souza@plus-it.com.br>
2020-02-24 09:01:33 -0300

    adição de classe base para gestão de retorno.

---
+++ src/HotelContext/Hotel.API/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.API.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+         public async Task<ActionResult> ResponseBase<T>(Task<T> func) where T : class
+        {
+            if (await func != null)
+            {
+                return Ok(func.Result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        public async Task<ActionResult> ResponseBase(Task func)
+        {
+            await func;
+            return Ok();
+        }
+    }
+}
